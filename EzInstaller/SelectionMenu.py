448bda95b526a47b72ceebe363be657e8ea9533f
radinals <radinalsaragih@gmail.com>
2024-09-28 19:56:03 +0700

    improved menu, added theme, tweaked rofi flags

--- EzInstaller/SelectionMenu.py
+++ EzInstaller/SelectionMenu.py
@@ -4,21 +4,24 @@ from .SpecialButtons import DownloadBtn, ExitBtn
 
 import csv
 
+import subprocess
+
 
 class DownloaderScriptEntry(MenuEntry):
-    def __init__(self, label, description, script_path):
+    def __init__(self, label, description, script_path, index=0):
         super().__init__(label)
         self.selected = False
         self.checklist = "[ ]"
         self.desc = description
         self.script_path = script_path
+        self.index = index
 
     def onSelected(self):
         if (self.selected):
             self.checklist = "[ ]"
             self.selected = False
         else:
-            self.checklist = "[X]"
+            self.checklist = "[✔️]"
             self.selected = True
 
     def getLabel(self):
@@ -26,12 +29,13 @@ class DownloaderScriptEntry(MenuEntry):
 
 
 class SelectionMenu(RunPromptWrapper):
-    def __init__(self, script_index_csv_path, script_path):
-        super().__init__("rofi -dmenu -no-sort")
+    def __init__(self, run_prompt_cmd, script_index_csv_path, script_path):
+        super().__init__(run_prompt_cmd)
         self.selected_scripts = []
         self.script_index_csv_path = script_index_csv_path
         self.script_path = script_path
         self.start_download = False
+        self.last_sel_row = 0
         self.generateMenuEntries()
 
     def hasStartDownload(self):
@@ -46,7 +50,27 @@ class SelectionMenu(RunPromptWrapper):
             self.start_download = True
 
         else:
+            # TODO: Magic Number '2'
+            if type(menu_entry) is DownloaderScriptEntry:
+                self.last_sel_row = abs(
+                    (len(self.menu_entries) - 2) - menu_entry.index)
             return super().onMenuEntrySelected(menu_entry)
+
+    def _execMenuCmd(self):
+        return subprocess.run(self.run_prompt_cmd + f" -selected-row {self.last_sel_row}",
+                              input=self._generateExecStr(),
+                              text=True,
+                              shell=True,
+                              capture_output=True)
+
+    def _captureEvent(self, exit_string):
+        if (exit_string):
+            menu_entry = self._getMenuEntry(
+                str(exit_string.stdout).strip("\n"))
+            if (menu_entry != None):
+                self.onMenuEntrySelected(menu_entry)
+            else:
+                exit()
 
     def generateMenuEntries(self):
 
@@ -55,12 +79,14 @@ class SelectionMenu(RunPromptWrapper):
 
         with open(self.script_index_csv_path, mode="r") as f:
             csvfile = csv.reader(f)
+            i = 1
             for lines in csvfile:
                 label = lines[0]
                 desc = lines[1]
                 script = f"{self.script_path}/{lines[2]}"
                 self.addMenuEntry(
-                    DownloaderScriptEntry(label, desc, script))
+                    DownloaderScriptEntry(label, desc, script, i))
+                i += 1
 
     def getSelectedScripts(self):
         return self.selected_scripts
--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from EzInstaller.DownloadProcess import DownloadProcess
 
 import os
 
+if __name__ == "__main__":
+    rofi_theme = f"{os.getcwd()}/EzInstallerRofi.rasi"
+    SelMenu = SelectionMenu(f"rofi  -dmenu -theme '{rofi_theme}' -p '' -markup -no-sort - hover-select - me-select-entry '' - me-accept-entry MousePrimary",
+                            f"{os.getcwd()}/InstallerScripts/index.csv",
+                            f"{os.getcwd()}/InstallerScripts")
 
-SelMenu = SelectionMenu(
-    f"{os.getcwd()}/InstallerScripts/index.csv", f"{os.getcwd()}/InstallerScripts")
+    while not SelMenu.hasStartDownload():
+        SelMenu.start()
 
-while not SelMenu.hasStartDownload():
-    SelMenu.start()
+    Downloader = DownloadProcess(
+        SelMenu.getSelectedScripts(),
+        f"{os.getcwd()}/InstallerScripts/ScriptRunner.sh")
 
-Downloader = DownloadProcess(
-    SelMenu.getSelectedScripts(), f"{os.getcwd()}/InstallerScripts/ScriptRunner.sh")
-
-Downloader.runScripts()
+    Downloader.runScripts()
