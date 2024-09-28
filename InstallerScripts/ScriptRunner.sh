f7bc8b71f098a2003dcd943116ecf78fc3c4b165
radinals <radinalsaragih@gmail.com>
2024-09-28 14:13:26 +0700

    testing scripts

--- InstallerScripts/ScriptRunner.sh
+++ InstallerScripts/ScriptRunner.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+count=1
+total_scripts="$#"
+
+printf "\n[%s]\n\n" "Starting Installation Process, do not close this window."
+
 while [ $# -gt 0 ]; do
     if [ -x "$1" ]; then
+        printf "%s\n" "# Executing Installer Script '$(basename $1)' ($count/$total_scripts) #"
         bash -e "$1"
+        count=$((count + 1))
     fi
     shift
 done
+
+printf "\n[%s]\n" "Starting Installation Completed, you can now safely close this window."
 
--- InstallerScripts/installer_vscode_apt.sh
+++ InstallerScripts/installer_vscode_apt.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
+printf "%s\n" "Syncing repositories"
+
 apt update
+
+printf "%s\n" "Installing Dependencies"
 
 apt install software-properties-common apt-transport-https wget -y
 
+printf "%s\n" "Installing Microsoft Repositories gpg key"
+
 apt install wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
 
+printf "%s\n" "Adding Microsoft Visual Studio Code Repositories"
+
 add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+printf "%s\n" "Installing Visual Studio Code"
 
 apt install code -y
