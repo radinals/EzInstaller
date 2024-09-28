e2ffb186a98752dc2b73ea491b1b2ab4412617c1
radinals <radinalsaragih@gmail.com>
2024-09-28 14:28:40 +0700

    testing installer script

--- InstallerScripts/installer_vscode_apt.sh
+++ InstallerScripts/installer_vscode_apt.sh
@@ -8,13 +8,13 @@ printf "%s\n" "Installing Dependencies"
 
 apt install software-properties-common apt-transport-https wget -y
 
-printf "%s\n" "Installing Microsoft Repositories gpg key"
+printf "%s\n" "Installing Microsoft Repositories and gpg key"
 
-apt install wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
-
-printf "%s\n" "Adding Microsoft Visual Studio Code Repositories"
-
-add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
 
 printf "%s\n" "Installing Visual Studio Code"
 
